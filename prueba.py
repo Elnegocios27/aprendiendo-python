1d52b380a45baf0bc0acc5d9ea4e46d2c132065d
Kenn27C <maximogaspar777@outlook.com>
2025-07-23 23:35:16 -0300

    calculadora

--- prueba.py
+++ prueba.py
@@ -1,2 +1,47 @@
-numeros = 2, 3, 4
-print("los numeros son %d", numeros)
+while True:
+    print("======================")
+    print("       calculadora     ")
+    print("======================")
+    print("1. Sumar")
+    print("2. Restar")
+    print("3. Multiplicar")
+    print("4. Dividir")
+    print("5. Salir")
+    print("======================")
+
+    def sumar(a, b):
+        return a + b
+    def restar(a, b):
+        return a - b
+    def multiplicar(a, b):
+        return a * b
+    def dividir(a, b):
+        if b != 0:
+            return a / b
+        else:
+            return "Error: Division por cero no permitida."
+    
+    opcion = input("elegí una opción (1-5):")
+    if opcion == '1':
+        num1 = float(input("primer numero:"))
+        num2 = float(input("segundo numero:"))
+        resultado = num1 + num2
+        print("resultado:", resultado)
+    if opcion == '2':
+        num1 = float(input("primer numero:"))
+        num2 = float(input("segundo numero:"))
+        resultado = num1 - num2
+        print("resultado:", resultado)
+    if opcion == '3':
+        num1 = float(input("primer numero:"))
+        num2 = float(input("segundo numero:"))
+        resultado = num1 * num2
+        print("resultado:", resultado)
+    if opcion == '4':
+        num1 = float(input("primer numero:"))
+        num2 = float(input("segundo numero:"))
+        resultado = num1 / num2
+        print("resultado:", resultado)
+    if opcion == '5':
+        print("saliendo de la calculadora")
+        break
