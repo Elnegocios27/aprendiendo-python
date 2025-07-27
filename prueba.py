553a72b972ada4a6a427b2c66b155fcb4e009bd9
Kenn27C <maximogaspar777@outlook.com>
2025-07-27 01:09:19 -0300

    con reutilización

--- prueba.py
+++ prueba.py
@@ -1,4 +1,19 @@
+def sumar(a, b):
+        return a + b
+def restar(a, b):
+        return a - b
+def multiplicar(a, b):
+        return a * b
+def dividir(a, b):
+        if b != 0:
+            return a / b
+        else:
+            return "Error: Division por cero no permitida."
+
+ultimo_resultado = None
+
 while True:
+
     print("======================")
     print("       calculadora     ")
     print("======================")
@@ -8,40 +23,54 @@ while True:
     print("4. Dividir")
     print("5. Salir")
     print("======================")
+    print("ans significa ultimo resultado")
 
-    def sumar(a, b):
-        return a + b
-    def restar(a, b):
-        return a - b
-    def multiplicar(a, b):
-        return a * b
-    def dividir(a, b):
-        if b != 0:
-            return a / b
-        else:
-            return "Error: Division por cero no permitida."
+    opcion = input("elegí una opción (1-5): ")
     
-    opcion = input("elegí una opción (1-5):")
-    if opcion == '1':
-        num1 = float(input("primer numero:"))
-        num2 = float(input("segundo numero:"))
-        resultado = num1 + num2
-        print("resultado:", resultado)
-    if opcion == '2':
-        num1 = float(input("primer numero:"))
-        num2 = float(input("segundo numero:"))
-        resultado = num1 - num2
-        print("resultado:", resultado)
-    if opcion == '3':
-        num1 = float(input("primer numero:"))
-        num2 = float(input("segundo numero:"))
-        resultado = num1 * num2
-        print("resultado:", resultado)
-    if opcion == '4':
-        num1 = float(input("primer numero:"))
-        num2 = float(input("segundo numero:"))
-        resultado = num1 / num2
-        print("resultado:", resultado)
     if opcion == '5':
         print("saliendo de la calculadora")
         break
+
+    entrada1 = input("ingrese el primer numero o ans: ")
+    if entrada1 == 'ans':
+        if ultimo_resultado is not None:
+                num1 = ultimo_resultado
+        else:
+            print("no hubo un resultado anterior")
+            continue
+    else:
+        try:
+            num1 = float(entrada1)
+        except ValueError:
+            print("entrada invalida")
+            continue
+
+    entrada2 = input("ingrese el primer numero o ans: ")
+    if entrada2 == 'ans':
+        if ultimo_resultado is not None:
+                num2 = ultimo_resultado
+        else:
+            print("no hubo resultado anterior")
+            continue
+    else:
+        try:
+             num2 = float(entrada2)
+        except ValueError:
+             print("entrada invalida")
+             continue
+
+    if opcion == '1':
+        resultado = sumar(num1, num2)
+    elif opcion == '2':
+        resultado = restar(num1, num2)
+    elif opcion == '3':
+        resultado = multiplicar(num1, num2)
+    elif opcion == '4':
+        resultado = dividir(num1, num2)
+    else:
+        print("opcion no valida")
+        continue
+
+    print("resultado: ", resultado)
+    if isinstance(resultado, (int, float)):
+         ultimo_resultado = resultado
