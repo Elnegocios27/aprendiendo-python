a7ec6f7915e28991762b7b8d4c4ad15c8d304fc4
unknown <maximogaspar777@outlook.com>
2026-08-07 22:15:20 -0300

    paso en blanco

--- objeto.py
+++ objeto.py
@@ -1,11 +1,8 @@
-import blessed
 import math
 from blessed import Terminal 
 from datetime import datetime
 term = Terminal() 
 allowed = "0123456789+-*/()., "
-
-date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 while True:
     expr = input(">>> ")
@@ -13,12 +10,16 @@ while True:
     if not all(c in allowed or c.isalpha() for c in expr):
         print("error syntaxis")
         continue
-
+    
+    result = None
     try:
         result = eval(expr, {"__builtins__": None}, vars(math))
         print(term.green(str(result)))
     except Exception as e:
         print(term.red(f"error in evaluation: {e}"))
 
-    with open("history.txt", "a") as f:
-        f.write(f"{date} - {expr} = {result}\n")
+    if result is not None:
+        date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        with open("history.txt", "a") as f:
+            f.write(f"{date} - {expr} = {result}\n")
+
